28b67814a19680070ebe22c2e4b2d6f264657254
MinotaurTheGreat <134445612+minotavi11@users.noreply.github.com>
2026-04-30 21:55:44 +0200

    fix: metro and nativewind crash on save, prettier on classes, easier component add

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,28 @@
 import { Button, ButtonText } from '@shared/components/ui/button';
+import { Image } from '@shared/components/ui/image';
 import { StyleSheet, View } from 'react-native';
 
 export default function Index() {
   return (
-    <View style={styles.container}>
+    <View className="bg-red-900" style={styles.container}>
+      <Image
+        size="2xl"
+        source={{
+          uri: 'https://fs.artdevivre.com/storage/articles/events-article/a-monet/fd4a2b62395c7281b28501fdc14421d6.jpg',
+        }}
+        alt="image"
+      />
       <Button
-        className="bg-blue-500"
+        className="bg-blue-500 "
         variant="solid"
         size="md"
         action="primary"
-        onPress={() => alert('Why did you click me?')}>
-        <ButtonText>{"Hello! Don't Click me Please!"}</ButtonText>
+        onPress={() =>
+          alert(
+            'Monet was a French painter, a founder of Impressionism, and is best known for his landscape paintings. The Stroll is one of his famous works, depicting a woman with a parasol walking in a field of flowers.',
+          )
+        }>
+        <ButtonText>{'Find out more'}</ButtonText>
       </Button>
     </View>
   );

d745f186ba403bc3921ce7711e9dc0c8199ff8a7
Vojtech Sanda <50047248+vojtechsanda@users.noreply.github.com>
2026-05-14 19:25:26 +0200

    feat(shared-components): add ScreenContainer component

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -1,10 +1,15 @@
 import { GluestackUIProvider } from '@providers/gluestack-ui-provider';
 import React, { ReactNode } from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 type AppProvidersProps = {
   children: ReactNode;
 };
 
 export function AppProviders({ children }: AppProvidersProps) {
-  return <GluestackUIProvider mode="light">{children}</GluestackUIProvider>;
+  return (
+    <SafeAreaProvider>
+      <GluestackUIProvider mode="light">{children}</GluestackUIProvider>
+    </SafeAreaProvider>
+  );
 }

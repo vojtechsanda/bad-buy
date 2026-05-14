17fecdc3d6d67869be7f0c11c73a78b6c18dc4bd
MinotaurTheGreat <134445612+minotavi11@users.noreply.github.com>
2026-05-14 03:23:42 +0200

    fix: improve basic conditional routing to be state aware

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,5 @@
+import { isLogged } from '@features/auth/store';
 import { Redirect } from 'expo-router';
-
-const isLogged = false;
 
 export default function Index() {
   return <Redirect href={isLogged ? '/(app)/home' : '/(auth)/landing'} />;

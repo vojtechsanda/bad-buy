382bf23f67fd979b05765d2461c96fcde8ab4a1f
Vojtech Sanda <50047248+vojtechsanda@users.noreply.github.com>
2026-05-17 16:37:25 +0200

    chore: rebase fix

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -2,6 +2,5 @@ export * from './error-message';
 export * from './form';
 export * from './layout';
 export * from './general';
-export * from './sheets';
 export * from './ui';
 export * from './sheets';

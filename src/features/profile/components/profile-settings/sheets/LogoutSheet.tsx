161e83d17c1e19ef96530cbceeec8245428b2a29
Vojtech Sanda <50047248+vojtechsanda@users.noreply.github.com>
2026-05-29 21:22:27 +0200

    feat: add error try catch around account and audit manipulation calls

--- src/features/profile/components/profile-settings/sheets/LogoutSheet.tsx
+++ src/features/profile/components/profile-settings/sheets/LogoutSheet.tsx
@@ -1,13 +1,20 @@
 import { authService } from '@features/auth';
 import { BottomSheet, BottomSheetProps, SheetActions, SheetHeader } from '@shared/components';
+import { Alert } from 'react-native';
 
 type LogoutSheetProps = Pick<BottomSheetProps, 'isOpen' | 'onClose'>;
 
 export function LogoutSheet({ isOpen, onClose }: LogoutSheetProps) {
   const handleLogout = async () => {
-    await authService.signOut();
+    try {
+      await authService.signOut();
 
-    onClose();
+      onClose();
+    } catch (e) {
+      console.error(JSON.stringify(e));
+
+      Alert.alert('Error', "Couldn't log out, please try again later.");
+    }
   };
 
   return (
